231b28ca8a680a952e8359a04111bf65da85d5cf
bergis952 <160930990+bergis952@users.noreply.github.com>
2025-09-22 17:30:07 -0500

    Ink Integration: Part the First

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,24 +3,30 @@ using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using System.Collections;
 using System.Collections.Generic;
+using Ink.Runtime;
+using Ink.UnityIntegration;
 
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager Instance;
+    [Header("UI Elements")]
     public Text nameText;
     public Text dialogueText;
+    [Header("Text Preferances")]
+    public float typingSpeed = 0.005f;
+    [Header("Ink Stuff")]
+    public TextAsset inkJSONAsset;
     private Queue<DialogueLine> lines;
     private SceneDetails details;
-    public float typingSpeed = 0.005f;
 
-    public enum DIALOGUESTATES
+    private Story inkStory;
+
+    private enum DIALOGUESTATES
     {
-        Writing,
-        Waiting,
-        Ready,
-        Done,
+        Writing,//Currently typing out the text
+        Ready,//Done typing, waiting for the player's next input
+        Done,//Done typing, no more lines in the scene
     }
-
     private DIALOGUESTATES dialogueState;
     private InputAction advance;
 
@@ -30,8 +36,8 @@ public class DialogueManager : MonoBehaviour
         {
             Instance = this;
         }
-        lines = new Queue<DialogueLine>();
-        advance = new InputAction(binding: "<Keyboard>/enter");
+
+        advance = new InputAction(binding: "<Keyboard>/enter");//Sets up are "go on" input. Currently only enter.
         advance.performed += ctx => PlayNextLine();
         advance.Enable();
     }
@@ -49,33 +55,46 @@ public class DialogueManager : MonoBehaviour
             Debug.LogWarning("No Dialogue found on " + scene.name);
             return;
         }
-
-        lines.Clear();
+        
         foreach (DialogueLine dialogueLine in details.dialogueLines) //And then uses those details to generate text
         {
-            lines.Enqueue(dialogueLine);
+            inkStory = new Story(inkJSONAsset.text);
         }
         dialogueState = DIALOGUESTATES.Ready;// Telling the manager that are lines are all ready
         PlayNextLine();
     }
     void PlayNextLine()
     {
-        if (lines.Count == 0){
-            dialogueState = DIALOGUESTATES.Done;
-            return;
+        if (inkStory.canContinue){
+            dialogueState = DIALOGUESTATES.Ready;
+
+            string nextLine = inkStory.Continue();
+
+            FetchCharacterDetails();
+
+            StartCoroutine(TypeSentence(nextLine));
         }
-
-        DialogueLine currentLine = lines.Dequeue();
-
-        nameText.text = currentLine.characterName;
-
-        StartCoroutine(TypeSentence(currentLine));
     }
 
-    IEnumerator TypeSentence(DialogueLine dialogueLine)
+    void FetchCharacterDetails(){
+        nameText.text = "";
+
+        foreach (string inkTag in inkStory.currentTags){
+
+            string[] split = inkTag.Split(":");
+            string key = split[0].Trim();
+            string value = split[1].Trim();
+
+            if (key == "character"){
+                nameText.text = value;
+            }
+        }
+    }
+
+    IEnumerator TypeSentence(string line)
     {
         dialogueText.text = "";
-        foreach (char letter in dialogueLine.line.ToCharArray())
+        foreach (char letter in line.ToCharArray())
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
