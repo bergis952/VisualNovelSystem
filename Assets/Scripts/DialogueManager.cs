e3b520163ff230339e97083e0b2e372dc7d56a0c
bergis952 <160930990+bergis952@users.noreply.github.com>
2025-10-02 16:01:19 -0500

    Choice system, part one.

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -14,20 +14,23 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text nameText;
     public TMP_Text dialogueText;
     public Image rightPortrait;
+    public GameObject buttonPrefab;
+    public VerticalLayoutGroup buttonContainer;
 //Temporary Demo stuff
     public Sprite standard;
     public Sprite happy;
     public Sprite angry;
-
+//End temp stuff
     [Header("Text Preferances")]
-    public float typingSpeed = 0.005f;
+    public float typingSpeed = 0.01f;
     [Header("Ink Stuff")]
-    public TextAsset inkJSONAsset;
+    public TextAsset inkJSONAsset;//This is the actual story asset. Will eventually be fed in through an event trigger
 
     private Story inkStory;
 
     private enum DIALOGUESTATES{
         Writing,//Currently typing out the text
+        Waiting,//Done typing, waiting for player to make choice
         Ready,//Done typing, waiting for the player's next input
         Done,//Done typing, no more lines in the scene
     }
@@ -42,11 +45,9 @@ public class DialogueManager : MonoBehaviour
         else {
             Destroy(gameObject);
         }
-    
-
+        //This sets up the Input system
         actions = new InputSystem_Actions();
-
-        actions.Dialogue.Advance.performed += ctx => PlayNextLine();
+        actions.Dialogue.Advance.performed += ctx => PlayNextLine();//calls the next line any time "advance" inputs are used (lmouse, enter, etc...)
         TriggerDialogue();
     }
     void OnEnable()
@@ -66,13 +67,9 @@ public class DialogueManager : MonoBehaviour
     }
     void PlayNextLine()
     {
-        if (inkStory.canContinue){
-            dialogueState = DIALOGUESTATES.Ready;
-
+        if (dialogueState == DIALOGUESTATES.Ready && inkStory.canContinue){
             string nextLine = inkStory.Continue();
-
             FetchCharacterDetails();
-
             StartCoroutine(TypeSentence(nextLine));
         }
     }
@@ -111,12 +108,42 @@ public class DialogueManager : MonoBehaviour
         }
     }
 
+    void DisplayChoices()
+    {
+        if (buttonContainer.GetComponentsInChildren<Button>().Length > 0)
+        {
+            Debug.Log("Choices have already been displayed.");
+        }
+        else
+        {
+            for (int i = 0; i < inkStory.currentChoices.Count; i++)
+            {
+                var choice = inkStory.currentChoices[i];
+                var button = Instantiate(buttonPrefab);
+                button.transform.SetParent(buttonContainer.transform, false);
+
+                var bText = button.GetComponentInChildren<TMP_Text>();
+                bText.text = choice.text; 
+            }
+        }
+    }
+
     IEnumerator TypeSentence(string line)
     {
+        dialogueState = DIALOGUESTATES.Writing;
         dialogueText.text = "";
         foreach (char letter in line.ToCharArray()){
             dialogueText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
+        }
+        if (inkStory.currentChoices.Count >= 1){
+            dialogueState = DIALOGUESTATES.Waiting;
+            DisplayChoices();
+        }
+        else
+        {
+            dialogueState = DIALOGUESTATES.Ready;
+
         }
     }
 }
